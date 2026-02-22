8039c9b32d1de71b2277efa152a65a3def339d1c
Tomil <68993838+93kazy@users.noreply.github.com>
2026-02-22 15:26:39 +0100

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,19 +1,17 @@
-import torch 
+import torch
 import torchvision
 import os
 import argparse
+import math
 
-
-from model import Generator
+from model import Generator, Discriminator
 from utils import load_model
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate Normalizing Flow.')
+    parser = argparse.ArgumentParser(description='Generate with Discriminator Driven Latent Sampling.')
     parser.add_argument("--batch_size", type=int, default=2048,
                       help="The batch size to use for training.")
     args = parser.parse_args()
-
-
 
     if torch.cuda.is_available():
         device = torch.device("cuda")
@@ -26,32 +24,47 @@ if __name__ == '__main__':
         print(f"Using device: CPU")
 
     print('Model Loading...')
-    # Model Pipeline
     mnist_dim = 784
 
-    model = Generator(g_output_dim=mnist_dim).to(device)
-    model = load_model(model, 'checkpoints', device)
+    model_G = Generator(g_output_dim=mnist_dim).to(device)
+    model_D = Discriminator(d_input_dim=mnist_dim).to(device)
+    
+    model_G, model_D = load_model(model_G, model_D, 'checkpoints', device)
+    
     if torch.cuda.device_count() > 1:
-        model = torch.nn.DataParallel(model)
-    model.eval()
-
-    print('Model loaded.')
-
-
+        model_G = torch.nn.DataParallel(model_G)
+        model_D = torch.nn.DataParallel(model_D)
+        
+    model_G.eval()
+    model_D.eval()
+    print('Models loaded.')
 
     print('Start Generating')
     os.makedirs('samples', exist_ok=True)
 
+    steps = 100
+    epsilon = 0.1
     n_samples = 0
-    with torch.no_grad():
-        while n_samples<10000:
-            z = torch.randn(args.batch_size, 100).to(device)
-            x = model(z)
-            x = x.reshape(args.batch_size, 28, 28)
+    while n_samples < 10000:
+        z = torch.randn(args.batch_size, 100).to(device)
+        for i in range(steps):
+            z.requires_grad_(True)
+            x = model_G(z)
+            d_out = model_D(x)
+            d = torch.logit(d_out, eps=1e-7).squeeze()
+            prior_energy = 0.5 * torch.sum(z**2, dim=1)
+            energy = prior_energy - d
+            grad_z = torch.autograd.grad(outputs=energy.sum(), inputs=z)[0]
+            noise = torch.randn_like(z)
+            with torch.no_grad():
+                z = z - (epsilon / 2) * grad_z + math.sqrt(epsilon) * noise
+                z = z.detach()
+
+        with torch.no_grad():
+            x = model_G(z)
+            x = x.reshape(args.batch_size, 1, 28, 28)
+            
             for k in range(x.shape[0]):
-                if n_samples<10000:
-                    torchvision.utils.save_image(x[k:k+1], os.path.join('samples', f'{n_samples}.png'))         
+                if n_samples < 10000:
+                    torchvision.utils.save_image(x[k], os.path.join('samples', f'{n_samples}.png'))         
                     n_samples += 1
-
-
-    
